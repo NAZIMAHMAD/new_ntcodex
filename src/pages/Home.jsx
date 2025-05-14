e48fc6e728137dc8025dd79d06d9cc40736f341e
nazim ahmad <zk8052272@gmail.com>
2025-05-14 23:47:32 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,17 @@ import img4 from '../assets/Images/img4.png'
 import img5 from '../assets/Images/img5.png'
 import img6 from '../assets/Images/img6.png'
 import img7 from '../assets/Images/img7.png'
+import img8 from '../assets/Images/img8.png'
+import img9 from '../assets/Images/img9.png'
+import img10 from '../assets/Images/img10.png'
+import img11 from '../assets/Images/img11.png'
+import img12 from '../assets/Images/img12.png'
+import img13 from '../assets/Images/img13.png'
+import img14 from '../assets/Images/img14.png'
+import img15 from '../assets/Images/img15.png'
+import img16 from '../assets/Images/img16.png'
+import img17 from '../assets/Images/img17.png'
+
 
 import { NavLink } from 'react-router-dom';
 
@@ -76,7 +87,7 @@ const Home = () => {
 
 {/* SECTION 2.  */}
 
-<section className='w-full h-auto py-12 bg-[#1d2549] lg:px-28 px-3 '>
+<section className='w-full h-auto py-12 bg-gradient-to-r from-[#1d2549] via-[#476504] to-[#1d2549] lg:px-28 px-3 '>
 
 <div className='container mx-auto h-auto grid grid-cols-1 gap-10 '>
 
@@ -165,13 +176,13 @@ Read More <GoArrowDownRight className='inline-block font-bold text-3xl'/>
 
 {/* SECTION 3.  */}
 
-<section className='w-full h-auto py-12 bg-[#1d2549] lg:px-28 px-3 '>
+<section className='w-full h-auto py-12 bg-gradient-to-r from-[#1d2549] via-[#476504] to-[#1d2549] lg:px-28 px-3 '>
 
 <div className='container mx-auto h-auto grid grid-cols-1 gap-10 '>
 
 <div className='grid grid-cols-1 items-center'>
 
-<div className='grid grid-cols-1 gap-4'>
+<div className='md:w-[70%] w-full mx-auto gap-4 flex justify-center flex-col text-center'>
   <p className=' text-white md:text-2xl text-2xl  font-bold'>SERVICES</p>
 
 <h3 className=' text-[#bdf347] md:text-4xl text-3xl  font-bold'>Transform your business with advanced technologies</h3>
@@ -250,11 +261,164 @@ Read More <GoArrowDownRight className='inline-block font-bold text-3xl'/>
 
 </div>
 
-
+<div className='grid grid-cols-1 '>
+ <button className='md:w-60 w-60 mx-auto  rounded-full  h-auto   p-3  font-bold capitalize text-xl mt-4 text-white border'>
+              view all topics <GoArrowDownRight className='inline-block font-bold text-3xl'/>
+            </button>
+</div>
 
 </div>
 
 
+
+</section>
+
+
+
+{/* SECTION 4. */}
+
+
+<section className='w-full lg:px-28 px-3 h-auto py-12 bg-gradient-to-r from-[#1d2549] via-[#476504] to-[#1d2549]'>
+
+
+<div className='container  mx-auto  grid grid-cols-1 gap-12'>
+
+<div className='md:w-[70%] w-full text-center mx-auto'>
+<h4 className=' text-[#bdf347] md:text-4xl text-3xl  font-bold'>Select your industry. Discover our impact.</h4>
+</div>
+
+<div className=' grid grid-cols-2 gap-8 '>
+
+<div className=' text-white gap-y-5 flex md:flex-row flex-col justify-center  items-center'>
+
+<div className='md:w-[20%] w-[50%]  '>
+<img src={img8} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto   text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>banking</p>
+</div>
+
+</div>
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img9} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Retail</p>
+</div>
+
+</div>
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img10} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Communications, Media, and Information Services</p>
+</div>
+
+</div>
+
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img11} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Travel & logistices</p>
+</div>
+
+</div>
+
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img12} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Education</p>
+</div>
+
+</div>
+
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img13} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Public services</p>
+</div>
+
+</div>
+
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img14} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Healthcare</p>
+</div>
+
+</div>
+
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img15} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>High tech</p>
+</div>
+
+</div>
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img16} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Insurance
+</p>
+</div>
+
+</div>
+
+<div className=' text-white gap-y-5  flex md:flex-row flex-col  items-center'>
+
+<div className='md:w-[20%] w-[50%] '>
+<img src={img17} alt="" className='w-[3rem] h-[3rem] mx-auto'/>
+</div>
+
+<div className='md:w-[80%] w-auto text-center md:text-start'>
+<p className='md:text-2xl text-xl capitalize font-semibold'>Consumer and good distribution</p>
+</div>
+
+</div>
+
+
+</div>
+
+</div>
 
 </section>
 
